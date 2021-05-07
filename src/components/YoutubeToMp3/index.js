de26b0a9dc6761d4788ef65eb838af553b826d7b
fabriciodelima06 <fabriciodelima06@gmail.com>
2021-05-07 13:09:12 -0300

    Youtube-to-mp3 v2

--- src/components/YoutubeToMp3/index.js
+++ src/components/YoutubeToMp3/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 
-export default props => {
+const YoutubeToMp3 = props => {
 
     return (
         <>
         <iframe 
+        title='Baixar Vídeo do youtube para mp3/mp4'
           src="https://mp3downy.com/MP3-converter?apikey=1234567890&color=17a2b8&bg=ffffff" 
           style={{width: '100%', minHeight: 250, height: 'auto'}} 
         //   style="width:100%;min-height:250px;height:auto;" 
@@ -13,3 +14,5 @@ export default props => {
         </>
     )
 }
+
+export default YoutubeToMp3;
